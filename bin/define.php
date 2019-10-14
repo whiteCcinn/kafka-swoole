fea005061c707c4d76a95e8fe8ec508e77fa6d5c
whiteCcinn <471113744@qq.com>
2019-10-14 15:39:07 +0800

    update define

--- bin/define.php
+++ bin/define.php
@@ -1,14 +1,10 @@
 <?php
 !defined('KAFKA_SWOOLE_ROOT') && define('KAFKA_SWOOLE_ROOT', dirname(__DIR__));
-!defined('KAFKA_SWOOLE_SRC') && define('KAFKA_SWOOLE_SRC', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
+!defined('KAFKA_SWOOLE_VENDOR') && define('KAFKA_SWOOLE_VENDOR', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
+!defined('KAFKA_SWOOLE_SRC') && define('KAFKA_SWOOLE_SRC',
+    KAFKA_SWOOLE_VENDOR . DIRECTORY_SEPARATOR . 'ccinn/kafka-swoole-core/src');
 !defined('KAFKA_SWOOLE_APP') && define('KAFKA_SWOOLE_APP', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app');
 !defined('KAFKA_SWOOLE_BIN') && define('KAFKA_SWOOLE_BIN', dirname(__DIR__) . DIRECTORY_SEPARATOR, 'bin');
 !defined('KAFKA_SWOOLE_CONFIG') && define('KAFKA_SWOOLE_CONFIG', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config');
-!defined('KAFKA_SWOOLE_CONFIG_HIGH_LEVEL_FILE') && define('KAFKA_SWOOLE_CONFIG_HIGH_LEVEL_FILE',
-    KAFKA_SWOOLE_CONFIG . DIRECTORY_SEPARATOR . 'high_level.yaml');
-!defined('KAFKA_SWOOLE_CONFIG_HIGH_LOW_FILE') && define('KAFKA_SWOOLE_CONFIG_LOW_LEVEL_FILE',
-    dirname(__DIR__) . DIRECTORY_SEPARATOR . 'low_level.yaml');
-!defined('KAFKA_SWOOLE_CONFIG_HIGH_PRODUCER_FILE') && define('KAFKA_SWOOLE_CONFIG_HIGH_PRODUCER_FILE',
-    dirname(__DIR__) . DIRECTORY_SEPARATOR . 'producer.yaml');
 !defined('KAFKA_SWOOLE_CONFIG_COMMON_FILE') && define('KAFKA_SWOOLE_CONFIG_COMMON_FILE',
     KAFKA_SWOOLE_CONFIG . DIRECTORY_SEPARATOR . 'common.yaml');
