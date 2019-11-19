0fe5ef122ae01e1c1ec6668353c9f96024bdb842
ccinn <471113744@qq.com>
2019-11-19 20:18:20 +0800

    Update CoreSubscriber.php

--- app/Subscriber/CoreSubscriber.php
+++ app/Subscriber/CoreSubscriber.php
@@ -13,18 +13,43 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class CoreSubscriber implements EventSubscriberInterface
 {
+
     /**
      * @return array
      */
     public static function getSubscribedEvents(): array
     {
         return [
-            SinkerOtherEvent::NAME => 'onSinkerOther'
+            SinkerOtherEvent::NAME          => 'onSinkerOther',
+            SetKafkaProcessNameEvent::NAME  => 'onSetKafkaProcessName',
+            SetSinkerProcessNameEvent::NAME => 'onSetSinkerProcessName',
+            SetMasterProcessNameEvent::NAME => 'onSetMasterProcessName',
         ];
     }
 
-    public function onSinkerOther(SinkerOtherEvent $event)
+    /**
+     * @param SetMasterProcessNameEvent $event
+     */
+    public function onSetMasterProcessName(SetMasterProcessNameEvent $event): void
     {
-        // Do something parallel to the sink task and remember to release control
+        swoole_set_process_name(env('APP_NAME') . '_' . env('APP_ID') . ':master');
+    }
+
+    /**
+     * @param SetKafkaProcessNameEvent $event
+     */
+    public function onSetKafkaProcessName(SetKafkaProcessNameEvent $event): void
+    {
+        $index = $event->getIndex();
+        swoole_set_process_name(env('APP_NAME') . '_' . env('APP_ID') . ':kafka_' . $index);
+    }
+
+    /**
+     * @param SetSinkerProcessNameEvent $event
+     */
+    public function onSetSinkerProcessName(SetSinkerProcessNameEvent $event): void
+    {
+        $index = $event->getIndex();
+        swoole_set_process_name(env('APP_NAME') . '_' . env('APP_ID') . ':sinker_' . $index);
     }
 }
