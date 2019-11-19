02d8f659a9893ae6704a6907892a482d8b491880
ccinn <471113744@qq.com>
2019-11-19 20:17:00 +0800

    Update HighLevelHandler.php

--- app/Handler/HighLevelHandler.php
+++ app/Handler/HighLevelHandler.php
@@ -6,7 +6,7 @@ use Kafka\Api\HighLevel\ConsumerInterface;
 
 class HighLevelHandler implements ConsumerInterface
 {
-    public function handler(string $message)
+    public function handler(string $topic, int $partition, int $offset, string $message)
     {
         echo '[--] Received a message that read:' . $message . PHP_EOL;
     }
