69861b8fb2c432345507a9a9eac42ccb04eb231d
ccinn <471113744@qq.com>
2019-11-19 20:19:09 +0800

    Update define.php

--- bin/define.php
+++ bin/define.php
@@ -4,7 +4,7 @@
 !defined('KAFKA_SWOOLE_SRC') && define('KAFKA_SWOOLE_SRC',
     KAFKA_SWOOLE_VENDOR . DIRECTORY_SEPARATOR . 'ccinn/kafka-swoole-core/src');
 !defined('KAFKA_SWOOLE_APP') && define('KAFKA_SWOOLE_APP', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app');
-!defined('KAFKA_SWOOLE_BIN') && define('KAFKA_SWOOLE_BIN', dirname(__DIR__) . DIRECTORY_SEPARATOR, 'bin');
+!defined('KAFKA_SWOOLE_BIN') && define('KAFKA_SWOOLE_BIN', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bin');
 !defined('KAFKA_SWOOLE_CONFIG') && define('KAFKA_SWOOLE_CONFIG', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config');
 !defined('KAFKA_SWOOLE_CONFIG_COMMON_FILE') && define('KAFKA_SWOOLE_CONFIG_COMMON_FILE',
     KAFKA_SWOOLE_CONFIG . DIRECTORY_SEPARATOR . 'common.yaml');
